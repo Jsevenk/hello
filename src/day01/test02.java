bd9718aec5886fd8fdccb31b358382284e2f7781
Administrator <Administrator@20171106-1233>
2017-11-29 10:57:24 +0800

    committed by who?

--- src/day01/test02.java
+++ src/day01/test02.java
@@ -4,5 +4,6 @@ public class test02 {
 	public static void main(String[] args) {
 		System.out.println("today is 2017.11.29");
 		System.out.println("remove");
+		System.out.println("add from jsevenk??");
 	}
 }
