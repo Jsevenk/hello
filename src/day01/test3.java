308b6a0797642bf1d7d464296480b68de671340e
Jsevenk <33485144+Jsevenk@users.noreply.github.com>
2017-12-06 21:08:52 +0800

    测试：at home

--- src/day01/test3.java
+++ src/day01/test3.java
@@ -2,7 +2,7 @@ package day01;
 
 public class test3 {
 	private void mian() {
-		// TODO Auto-generated method stub
-		System.out.println("字体？");
+		System.out.println("字体?");
+		System.out.println("上传可以吗?");
 	}
 }
