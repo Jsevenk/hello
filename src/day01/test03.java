3d6a7c9a9d0c906f192fc1971b893abdd4bc6229
Jsevenk <2446359217@qq.com>
2017-12-07 17:00:07 +0800

    cs：jdk (home and corporation)

---
+++ src/day01/test03.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class test03 {
+	private void mian() {
+		System.out.println("字体?");
+		System.out.println("上传可以�?");
+		System.out.println("啦啦啦，等下在家测一下接受问题，和jdk切换问题.");
+	}
+}
