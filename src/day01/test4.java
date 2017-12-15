b43210f570802d1824cca1203cf41a13aa0bc380
Jsevenk <2446359217@qq.com>
2017-12-15 15:09:30 +0800

    split("\\")

---
+++ src/day01/test4.java
@@ -0,0 +1,18 @@
+package day01;
+
+public class test4 {
+
+	public static void main(String[] args) {
+		   String[] aa = "aaa|bbb|ccc".split("|");
+
+//		    String[] aa = "aaa|bbb|ccc".split("\\|"); //这样才能得到正确的结果
+
+		    for (int i = 0 ; i <aa.length ; i++ ) {
+
+		      System.out.println("--"+aa[i]); 
+
+		    } 
+
+	}
+
+}
