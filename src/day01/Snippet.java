e1dd22359c5c7d3fa45a2f26b7b1f0e9ed29d407
Administrator <Administrator@20171106-1233>
2017-12-17 09:44:18 +0800

    11.28

---
+++ src/day01/Snippet.java
@@ -0,0 +1,40 @@
+package day01;
+
+import java.util.TreeMap;
+
+//import com.icinfo.hxpermit.genadim.datapoolingpackage.Message;
+//import com.icinfo.hxpermit.genadim.datapoolingpackage.PoolingInit;
+//import com.icinfo.hxpermit.genadim.datapoolingpackage.PoolingLicenseInfoDataClient;
+
+public class Snippet {
+	/*public static void main(String[] args) {
+		PoolingInit poolingInit = new PoolingInit();
+        poolingInit.setSignUrl("http://59.202.29.108");
+        poolingInit.setTimeUrl("http://59.202.29.106");
+        poolingInit.setSignType("SM2");
+        poolingInit.setAppId("CS_eehaeb7y6d");
+        poolingInit.setAppSecret("987654321");
+//        PoolingLicenseInfoDataClient dataClient = new PoolingLicenseInfoDataClient(poolingInit);
+        TreeMap<String, Object> paramMap = new TreeMap<String, Object>();
+        paramMap.put("MNAME","CSRM");
+        paramMap.put("MCSRQ","2017-03-22");
+        paramMap.put("MSFNO","420423199009081102");
+        paramMap.put("FZJG","测试浙江省数管中心");
+         
+//        Message message= dataClient.doGetPoolingSignData(paramMap,"测试浙江省数管中心","FZJG");
+//        if(message.isSuccess()){
+//        	message.getMsg();
+//        	System.out.println(message.getMsg());
+//	    }else{
+//	    	System.out.println(message.getMsg());
+//	    }
+        //判断是否是委托模式
+        //Message类中感觉少了一个方法，
+        if(paramMap.get("FZJG").equals("测试浙江省数管中心"))
+        	System.out.println(0);
+        else
+        	System.out.println(1);
+        System.out.println(paramMap.get("FZJG").equals("测试浙江省数管中心"));
+	}*/
+}
+
