ff7b4f2796a43d0820acb0fc937a1b0562cae4d0
Jsevenk <2446359217@qq.com>
2017-12-19 14:11:53 +0800

    substring

--- src/day01/test05.java
+++ src/day01/test05.java
@@ -6,7 +6,10 @@ public class test05 {
 		//蛇皮代码-,-|||
 		String String ="QAQ";
 		System.out.println(String);
-		
+		//substring用法:从0开始,截取位数
+		String ctime2="2017-10-10";
+		ctime2=ctime2.substring(0,7);
+		System.out.println(ctime2);
 	}
 
 }
