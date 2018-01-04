96ffa11b41912fba937ba32f98ce32bb404e51c7
Jsevenk <33485144+Jsevenk@users.noreply.github.com>
2018-01-04 19:37:34 +0800

    busy

--- src/day01/test06.java
+++ src/day01/test06.java
@@ -6,6 +6,7 @@ public class test06 {
 		System.out.println("change the computer!!");
 		System.out.println("�ܹ�ʦ��Ƶ���������Ŷ����");
 		System.out.println("test from home");
+		System.out.println("game");
 	}
 
 }
