bb6a550a59d2bafd68fa455d8b95fafae3413262
Jsevenk <2446359217@qq.com>
2018-01-11 13:58:59 +0800

    string

---
+++ src/day02/test04.java
@@ -0,0 +1,14 @@
+package day02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class test04 {
+	public static void main(String[] args) {
+		Map<String,String> map=new HashMap<>();
+		map.put("1", "v");
+		map.put("2", "c");
+		map.put("3", "x");
+		
+	}
+}
