ab1fcf2a1c2955cacb14d298634203481029581d
Jsevenk <2446359217@qq.com>
2018-01-18 16:12:18 +0800

    bigint

--- src/day03/test01.java
+++ src/day03/test01.java
@@ -1,5 +1,7 @@
 package day03;
 
+import java.math.BigInteger;
+
 public class test01 {
 	public static void main(String[] args) {
 		String a="";
@@ -12,5 +14,14 @@ public class test01 {
 		String aaa="012345";
 		String b=aaa.substring(0, 2);
 		System.out.println(b);
+		
+		System.out.println("=======================other=======================");
+		BigInteger bigint=null;
+		if(bigint!=null){
+			System.out.println("not null");
+		}else{
+			System.out.println("null");
+		}
+		
 	}
 }
