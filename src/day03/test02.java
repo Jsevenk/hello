2db4a6abd9a086359f1d1bfc74fce785b909b1b0
Jsevenk <2446359217@qq.com>
2018-01-24 11:07:46 +0800

    1

---
+++ src/day03/test02.java
@@ -0,0 +1,18 @@
+package day03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test02 {
+	public static void main(String[] args) {
+		System.out.println("=========ÎÞÊý¾Ý===========");
+		List itemList=new ArrayList();
+		for(int i=0;i<itemList.size();i++){
+			System.out.println(i);
+		}
+		String[] a=new String[]{};
+		for(int i=0;i<a.length;i++){
+			System.out.println(i);
+		}
+	}
+}
