d5476d3e453ebc901304c65c8fadf8ac0a908c00
Jsevenk <2446359217@qq.com>
2018-03-29 09:18:29 +0800

    1

---
+++ src/day04/test06.java
@@ -0,0 +1,18 @@
+package day04;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class test06 {
+
+	public static void main(String[] args) {
+		Map map =new HashMap<>();
+		List list=new ArrayList<>();
+		for(int i=0;i<list.size();i++){
+			System.out.println("===="+i+"=====");
+        }
+	}
+
+}
