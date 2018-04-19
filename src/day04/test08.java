e02f7aebf7701edcbaae985779dbcad47e5e0e6a
Jsevenk <2446359217@qq.com>
2018-04-19 16:20:18 +0800

    编码

---
+++ src/day04/test08.java
@@ -0,0 +1,39 @@
+package day04;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+public class test08 {
+
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		System.out.println("==============gbk->utf-8==============");
+		//java加密
+		String aa=URLEncoder.encode("陈婷婷","GBK");
+		//java解密	
+		String name = URLDecoder.decode(aa, "UTF-8");
+		System.out.println(name);
+		System.out.println("============================");
+		String bb=URLEncoder.encode(name,"UTF-8");
+		//java解密	
+		String name0 = URLDecoder.decode(bb, "GBK");
+		System.out.println(name0);
+		System.out.println("==============utf->gbk==============");
+		//2
+		//java加密
+		String dd=URLEncoder.encode("陈婷婷","UTF-8");
+		//java解密	
+		String name1 = URLDecoder.decode(dd, "GBK");
+		System.out.println(name1);
+		
+		System.out.println("============================");
+		String cc=URLEncoder.encode(name1,"GBK");
+		String name2 = URLDecoder.decode(cc, "UTF-8");
+		System.out.println(name2);
+		System.out.println("============================");
+		
+		
+		
+	}
+
+}
